31a1997bef54b3a37f8f9f5a5b57d8b72a2d6766
for-dream <469393425@qq.com>
2017-12-24 22:17:27 +0800

    Create 变态跳台阶.cpp

---
+++ DP/变态跳台阶.cpp
@@ -0,0 +1,22 @@
+/*
+https://www.nowcoder.com/practice/22243d016f6b47f2a6928b4313c85387?tpId=13&tqId=11162&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking&tPage=1
+题目描述
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+
+因为n级台阶，第一步有n种跳法：跳1级、跳2级、到跳n级
+跳1级，剩下n-1级，则剩下跳法是f(n-1)
+跳2级，剩下n-2级，则剩下跳法是f(n-2)
+所以f(n)=f(n-1)+f(n-2)+...+f(1)
+因为f(n-1)=f(n-2)+f(n-3)+...+f(1)
+所以f(n)=2*f(n-1)
+
+2^(n-1)
+*/
+
+class Solution {
+public:
+    int jumpFloorII(int number) {
+         int a=1; 
+        return a<<(number-1);
+    }
+};
